0ff4414a8eae2205ef1b1f485da6d2ce3718615a
Iryna_Kamotskaya <iryna_kamotskaya@epam.com>
2023-02-05 21:09:37 +0300

    Python Basics Home task

---
+++ Python_Basics/Python_Basics_Task.py
@@ -0,0 +1,45 @@
+with open('Random_list.txt') as list_file:      # open file for reading
+    file_cont = list_file.read()                # read file content into the variable
+rl = file_cont.split(',')                       # create the list from file content
+
+for i in range(len(rl)):                        # convert all list elements from str to int
+    rl[i] = int(rl[i])
+
+print('Input list - ', rl)                      # print input list
+
+####sort list from min to max (without using sort())#########
+for i in range(len(rl)):                        # loop to access each element in the list
+    for j in range(len(rl)-1-i):
+        if rl[j] > rl[j+1]:                     # compare two elements in the list
+            a = rl[j]                           # replace elements if condition for comparison is true
+            rl[j] = rl[j+1]
+            rl[j+1] = a
+print('Sorted list - ', rl)                     # print sorted list
+
+####calculate average for even and odd numbers (case 1)
+even_list = []                                  # define list for even values
+odd_list = []                                   # define list for odd values
+for el in rl:                                   # loop access each element in the list
+    if el % 2 == 0:                             # check if the element even or not
+        even_list.append(el)                    # if the element even add element in even_list
+    else:
+        odd_list.append(el)                     # if the element odd add element in odd_list
+even_avg = sum(even_list) / len(even_list)      # calculate average for even values
+odd_avg = sum(odd_list) / len(odd_list)         # calculate average for odd values
+print('Average for even ', even_avg)            # print result
+print('Average for odd ', odd_avg)
+
+####calculate average for even and odd numbers (case 2)
+ever_sum = 0                                    # define the input variables
+ever_count = 0
+odd_sum = 0
+odd_count = 0
+for el in rl:
+    if el % 2 == 0:                             # check if the element even or not
+        ever_sum = ever_sum + el                # calculate sum of even elements
+        ever_count += 1                         # calculate count of even elements
+    else:
+        odd_sum = odd_sum + el                  # calculate sum of odd elements
+        odd_count += 1                          # calculate count of odd elements
+print('Average for even ', ever_sum / ever_count)
+print('Average for odd ', odd_sum / odd_count)
